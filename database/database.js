860b245f96ce10963a686a97c30ee517c268dfb8
Ruan <ruanalmeidawegner@gmail.com>
2021-11-06 16:41:12 -0300

    heroku

--- database/database.js
+++ database/database.js
@@ -1,6 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const Sequelize = require("sequelize");
-const connection = new Sequelize(process.env.DATABASE_URL,{
-    dialect:'postgres'
+const connection = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASSWORD,{
+    host:process.env.DATABASE_HOST,
+    dialect:'postgres',
+    ssl: true
 })
 
 module.exports = connection;
