b24313193ac622939c0bf6744c0d8fc42b39e220
Ruan <ruanalmeidawegner@gmail.com>
2021-11-06 18:36:37 -0300

    heroku e localhost

--- database/database.js
+++ database/database.js
@@ -1,16 +1,26 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const Sequelize = require("sequelize");
-const connection = new Sequelize("DATABASE_NAME","DATABASE_USER","DATABASE_PASSWORD",{
-    host:"DATABASE_HOST",
-    dialect:'postgres',
-    protocol:'postgres',
-    dialectOptions:{
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+let connection = 'string de conexão';
+
+if (process.env.DATABASE_URL) {
+    connection = new Sequelize(process.env.DATABASE_URL,{
+        dialect:'postgres',
+        protocol:'postgres',
+        dialectOptions:{
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-});
+    });
+}else {
+    // the application is executed on the local machine
+    connection = sequelize = new Sequelize('perguntaserespostas', 'postgres', 'postgres',{
+        host:'localhost',
+        dialect:'postgres'
+    });
+  }
 
 module.exports = connection;
+  
